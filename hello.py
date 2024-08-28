fa8f16c74086877a85fceac168f9a93990a9bc88
mcarolinaa <mariac.andrea@yahoo.com>
2024-08-28 18:11:34 -0400

    Implementation of hello world multilang

--- hello.py
+++ hello.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
+"""
+Hello world multi languagues
 
-print(56525/2255)
+Depending on the configured language
+inside the environment, the program will
+exhibit the corresponding message
+
+Usage:
+Have the LANG environment variable properly
+set.
+ex:
+    export LANG=pt_br
+    
+Execution:
+python3 hello.py
+or
+./hello.py
+
+"""
+
+# special metadata
+__version__ = "0.0.1"
+__author__ = "MCarolina"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello world multi"
+
+if current_language == "pt_BR":
+    msg =  "Olá mundo multi"
+elif current_language == "fr_FR":
+    msg = "Salut monde multi"
+elif current_language == "es_SP":
+    msg = "Hola, mundo multi"
+elif current_language == "ru_RU":
+    msg = "privet, mul'ti mir"
+
+print(msg)
+
+
+
+
+
+
+
+
+
