2b6bec4b52e474e34c939d1697a7a559f980e55f
mcarolinaa <mariac.andrea@yahoo.com>
2024-09-20 08:48:57 -0400

    modify to add file systems

--- prefixcalc.py
+++ prefixcalc.py
@@ -26,13 +26,16 @@ n1: 5
 n2: 4
 9
 
+Os resultados serão salvos em `prefixcalc.log`
 """
 
 __version__ = "0.1.0"
 __author__ = "MCarolina"
 __license__ = "Unlicense"
 
+import os
 import sys
+from datetime import datetime
 
 arguments = sys.argv[1:] # pega os args q usuario digitar, exceto nome do progr
 
@@ -73,6 +76,15 @@ elif operation == "sub": result = n1 - n2
 elif operation == "mul": result = n1 * n2
 elif operation == "div": result = n1 / n2
         
+        
+path = os.curdir
+filepath = os.path.join(path, "prefixcalc.log") 
+timestamp = datetime.now().isoformat()
+user = os.getenv('USER', 'anonymous')
+
+with open(filepath, "a") as file_:
+    file_.write(f"{timestamp} - {user} - {operation}, {n1}, {n2} = {result}\n")
+
 print(f"O resultado é = {result}")
     
 
