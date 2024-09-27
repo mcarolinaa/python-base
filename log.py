bb1f232d10cf449b2eeff178ccf44f19bc0ea837
mcarolinaa <mariac.andrea@yahoo.com>
2024-09-27 07:55:58 -0400

    changed handlers of log

--- log.py
+++ log.py
@@ -2,6 +2,7 @@
 
 import os
 import logging
+from logging import handlers
 
 
 # boilerplate
@@ -10,14 +11,22 @@ import logging
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 # our instance - default: only shows from warning up (error and critical)
 log = logging.Logger(__name__, log_level) # debug or 10
-ch = logging.StreamHandler() # handler: write in my destination of choice
-ch.setLevel(log_level)
+# ch = logging.StreamHandler() # handler: write in my destination of choice
+# ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log",
+    maxBytes=300, # recommended = 10**6
+    backupCount=10 # num of backup files
+    )
+fh.setLevel(log_level)
 fmt = logging.Formatter(    # formating
     '%(asctime)s %(name)s %(levelname)s'
     'l:%(lineno)d f:%(filename)s %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+# ch.setFormatter(fmt)
+# log.addHandler(ch)
+fh.setFormatter(fmt)
+log.addHandler(fh)
 
 # what can be adjusted when using the logging module: (above)
 # level
